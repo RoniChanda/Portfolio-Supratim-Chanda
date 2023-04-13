c1960be472ecd601ce0fffe3812bdd65ee6ea4e4
Supratim <suprachanda97@gmail.com>
2023-04-13 10:21:33 +0530

    Animation time changes

--- src/components/AboutComp/MyDescription.js
+++ src/components/AboutComp/MyDescription.js
@@ -6,17 +6,17 @@ export default function MyDescription() {
       <p>
         I'm a full stack developer from Tripura, India. My main focus is on
         React and Mern Stack, but still exploring other technologies and
-        frameworks that catch my interest. I love creating web pages and react
-        native apps where I'm able to create enjoyable interactions and
-        experiences for everyone. Apart from this, I also enjoy watching and
-        applying various animations.
+        frameworks that catch my interest. I love creating web pages where I'm
+        able to create enjoyable interactions and experiences for everyone. I
+        enjoy designing web pages with lots of animations and highly interested
+        in learning 3D animated graphics.
       </p>
       <p>
-        As a fresher, I'm actively looking for an organization to implement my
-        skills, work as a team member, gain experience and get acquainted with
-        more tools and technologies. Graduated in 2019 with a bachelor's degree
-        in Electronics and in 2021 secured 161 AIR in GATE. From 2022, I have
-        been learning web development and working on design and mern{" "}
+        I'm actively looking for an organization to implement my skills, work as
+        a team member, gain experience and get acquainted with more tools and
+        technologies. Graduated in 2019 with a bachelor's degree in Electronics
+        and in 2021 secured 161 AIR in GATE. From 2022, I have been learning web
+        development and working on design and mern{" "}
         <a href="#projects" className="decription-link">
           projects.
         </a>
